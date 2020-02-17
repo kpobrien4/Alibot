bacd7a369249eddc7a4173962ec620b076db1d17
Kevin O'Brien <kpobrien@buffalo.edu>
2020-02-16 21:11:29 -0500

    random case created

--- alibot.py
+++ alibot.py
@@ -2,20 +2,14 @@ import tweepy
 from keys import *
 import sys
 import time
-from random import random
-import os
-from os import environ
 import json
 import requests
 import random
 from alibi import *
 
-# CONSUMER_KEY = os.environ.get('CONSUMER_KEY')
-# CONSUMER_SECRET = os.environ.get('CONSUMER_SECRET')
-# ACCESS_KEY = os.environ.get('ACCESS_KEY')
-# ACCESS_SECRET = os.environ.get('ACCESS_SECRET')
 
 resp = requests.get(api_url)
+desc = random.choice(resp.json())["pd_desc"]
 for data in resp.json():
     crime_description = data["pd_desc"]
 
@@ -27,7 +21,7 @@ auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
 def alibi():
-    return(random.choice(alibis) + ", NOT committing " + crime_description.lower())
+    return(random.choice(alibis) + ", NOT committing " + desc.lower())
 
 
 while True:
